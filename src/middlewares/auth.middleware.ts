582ddd1e244ea9a6a233085150922f3bb16750ad
jithinlal <jithtitan007@gmail.com>
2021-09-18 13:45:40 +0530

    feat: auht

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,39 +1,22 @@
-import { Request, NextFunction, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import * as admin from 'firebase-admin';
 import HttpException from '../exceptions/HttpException';
-import { DataStoredInToken } from '~/interfaces/auth.interface';
-import { User } from '~/models/User';
 
-async function authMiddleware(
-	req: Request,
-	_res: Response,
-	next: NextFunction
-) {
-	const cookies = req.cookies;
-
-	if (cookies && cookies.Authorization) {
-		const secret = process.env.JWT_SECRET;
-
-		try {
-			const verificationResponse = jwt.verify(
-				cookies.Authorization,
-				secret as jwt.Secret
-			) as DataStoredInToken;
-			const userId = verificationResponse.id;
-
-			const findUser = await User.findOne({ id: userId });
-			if (findUser) {
-				req.user = findUser;
-				next();
-			} else {
-				next(new HttpException(401, 'Wrong authentication token'));
-			}
-		} catch (error) {
-			next(new HttpException(401, 'Wrong authentication token'));
+async function authenticate(req: Request, _res: Response, next: NextFunction) {
+	const token = req.headers['authentication']! as string;
+	try {
+		if (!token) {
+			next(new HttpException(404, 'Authentication token missing'));
 		}
-	} else {
-		next(new HttpException(404, 'Authentication token missing'));
+
+		req.user =  await admin
+			.auth()
+			.verifyIdToken(token);
+		next();
+	} catch (error) {
+		console.log(error);
+		next(new HttpException(401, 'Wrong authentication token'));
 	}
 }
 
-export default authMiddleware;
+export default authenticate;
