f0aca0c0c9dc62b45b451b3e982826fc685e7465
jithinlal <jithtitan007@gmail.com>
2021-09-20 19:13:05 +0530

    feat: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# npm run build
+tar -czvf dist.tar.gz ./dist
+sftp -i /home/ntpl-297/Documents/Project-Docs/mentorship/techclub.pem ubuntu@ec2-65-2-129-210.ap-south-1.compute.amazonaws.com << !
+ cd lawoup/backend
+ put dist.tar.gz
+ bye
+!
+echo "replacing build"
+ssh ubuntu@ec2-65-2-129-210.ap-south-1.compute.amazonaws.com -i /home/ntpl-297/Documents/Project-Docs/mentorship/techclub.pem << !
+ export NVM_DIR=~/.nvm
+ source ~/.nvm/nvm.sh
+ cd lawoup/backend
+ rm -rf dist
+ tar -xzvf dist.tar.gz
+ rm dist.tar.gz
+ cp .env* ./dist/
+ pm2 restart all
+ echo "build completed"
+!
+rm dist.tar.gz
+/usr/bin/printf "\xE2\x9C\x94 build completed\n"
